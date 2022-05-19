aeca25eb471439e6c5631e3908835b9ec66ee414
azmi2409 <azmimuwahid@gmail.com>
2022-05-19 22:56:40 +0700

    push heroku

--- main.go
+++ main.go
@@ -3,17 +3,23 @@ package main
 import (
 	"api-store/config"
 	"api-store/routes"
+	"api-store/utils"
 	"fmt"
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
 	//programmatically set swagger info
 
-	// err := godotenv.Load()
-	// if err != nil {
-	// 	fmt.Println(err)
-	// }
+	enviroment := utils.GetEnv("ENVIROMENT", "development")
 
+	if enviroment == "development" {
+		err := godotenv.Load()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
 	db := config.ConnectDB()
 	sqlDB, _ := db.DB()
 	defer sqlDB.Close()
